babecdea04349b8b9c0e903843fac3612cf7b507
noorsiddiqui-prog <noorsiddiqui048@gmail.com>
2025-12-10 11:00:09 +0500

    feat: class 38 material added

---
+++ Javascript/class-38/index.js
@@ -0,0 +1,84 @@
+// console.log("hello world!");
+
+// alert("Hello Barbie!");
+
+
+// DataTypes
+
+//1.  String
+
+"Noor 123"
+"123545654646"
+'98379847239487gdfgdfgdfgd3'
+
+//1.  Numbers / int
+
+498274983747
+
+// float -> decimal
+
+2445.94857934
+
+// Undefined -> unassigned
+
+Name:
+
+// Null -> absence of value
+Name: null
+
+
+// "10" + 2
+// 10 + 2
+
+// object
+{
+Name: "Noor",
+Age: "100 years",
+Class: "Batch 18",
+Occupation: "Software Developer",
+Gender: "female",
+}
+
+// array []
+
+// [1,2,3,4,5,6,7,8,98,546,656546,345435,"sdfsdfsdf",true]
+
+[9,3,434,84,4535,]
+
+[1,2,3,4]
+["name", "age", "fruit", "gender"]
+["noor", "50 years", "apple", "female"]
+[true, false, true, false, true, false]
+[435345, "noor", true, 50.98]
+[
+    {
+        Name: "Noor",
+        Age: "100 years",
+        Class: "Batch 18",
+        Occupation: "Software Developer",
+        Gender: "female",
+    },
+    {
+        Name: "Sana",
+        Age: "200 years",
+        Class: "Batch 18",
+        Occupation: "Trainer",
+        Gender: "female",
+    },
+    {
+        Name: "Noor",
+        Age: "100 years",
+        Class: "Batch 18",
+        Occupation: "Software Developer",
+        Gender: "female",
+    },
+    {
+        Name: "Sana",
+        Age: "200 years",
+        Class: "Batch 18",
+        Occupation: "Trainer",
+        Gender: "female",
+    },
+    [34,334, "noor", true]
+
+]
---
+++ Javascript/class-38/script.js
@@ -0,0 +1,37 @@
+// var, let, const
+
+
+var studentsTable = [
+    {
+        Name: "Noor",
+        Age: "100 years",
+        Class: "Batch 18",
+        Occupation: "Software Developer",
+        Gender: "female",
+    },
+    {
+        Name: "Sana",
+        Age: "200 years",
+        Class: "Batch 18",
+        Occupation: "Trainer",
+        Gender: "female",
+    },
+    {
+        Name: "Noor",
+        Age: "100 years",
+        Class: "Batch 18",
+        Occupation: "Software Developer",
+        Gender: "female",
+    },
+    {
+        Name: "Sana",
+        Age: "200 years",
+        Class: "Batch 18",
+        Occupation: "Trainer",
+        Gender: "female",
+    },
+    [34,334, "noor", true]
+
+]
+
+console.log(studentsTable)
