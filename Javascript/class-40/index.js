95d5bf972b7d1202bb41378f226ea2834333d6a5
noorsiddiqui-prog <noorsiddiqui048@gmail.com>
2025-12-17 10:48:46 +0500

    feat: class 40 material added

---
+++ Javascript/class-40/index.js
@@ -0,0 +1,166 @@
+// var
+
+var x; // only declare variable
+var y;
+var description = "Noor SMIT Trainer and software developer and hello"
+var age = 500
+var phone = 90078601
+var isGenderFemale = true
+
+
+
+
+console.log("Pehli description", description)
+
+
+y = 0
+
+
+
+console.log("X variable", x)
+console.log("Y variable", y)
+
+
+
+console.log("2nd description", description)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("3rd description", description)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+description = "Updated My New String"
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("4th description", description)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("5th description", description)
+
+
+
+
+
+console.log("6th description", description)
+
+
+var description = "New Description BRO!"
+
+
+
+console.log("7th description", description)
+
+
+
+console.log("8th description", description)
+
+
+
+description = "Hello Bro!"
+
+
+
+
+
+console.log("description", description)
+
+// ES6 Javascript latest version
+//let
+
+let name = "Noor Ul Ain Siddiqui"
+
+console.log("name", name)
+
+
+name = "Sana"
+
+console.log("name 2", name)
+
+
+// const
+
+
+// const z; // CANNOT DECLARE VARIABLE WITH CONST, I have to initialize it in same line.
+
+const z = 1000
+
+console.log(z, "Z variable")
+
+
+
+
+
+
+
+// const z = 5000 // CANNOT RE DECLARE CONST variable
+// z = 5000 // CANNOT UPDATE THIS VARIABLE
+console.log("Updated Z variable", z)
+
+
+
+
+// let name = "Herry Potter" // CANNOR NOT RE_DECLARE VARIABLE WITH LET but it can be update
+
+name = "blah blah!!"
+console.log("name3", name)
+
+
+
+const nic  = 32423579847
+
+let age = 50
+
+age = 51
+age = 52
