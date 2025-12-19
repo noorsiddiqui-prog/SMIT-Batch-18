da97d5713c9064ea5e36888524a5987ea1836145
noorsiddiqui-prog <noorsiddiqui048@gmail.com>
2025-12-19 11:01:17 +0500

    feat: class 41 material added

---
+++ Javascript/class-41/index.js
@@ -0,0 +1,80 @@
+// var, let, const
+
+// var
+
+// console.log("alert0", confirm("Are you female?") )
+// console.log("alert", false)
+
+// const popup = confirm("Are you ok?")
+// console.log("popup show krdo", popup)
+
+// const promptModal = prompt("What is you age?", 10)
+
+// console.log("age btao bhai?", promptModal)
+
+
+// console.log("age btao bhai?", promptModal)
+
+
+// console.log("age btao bhai?", promptModal)
+
+
+// const sanaAge = 20
+
+// const ayeshaAge = sanaAge
+
+// console.log("SanaAge and ayeshaAge", sanaAge, ayeshaAge)
+
+
+// var -> ES5, 
+// let, const -> ES6
+
+var age = 10;
+
+console.log("age", age)
+
+
+
+var age = 20;
+
+console.log("junior age", age )
+
+
+console.log("duabara age usage", age)
+
+
+var name1;
+
+// data is getting from API -> "Noor"
+
+name1 = "Noor"
+
+console.log("name1", name1)
+
+
+// Let
+
+let y;
+
+
+console.log("y", y)
+
+y = 1000
+console.log("y2", y)
+
+y = 3000
+console.log("y2", y)
+
+// const z;
+
+// z = 20
+
+// console.log("z", z)
+
+const z = 9000
+
+console.log("z", z)
+
+// z = 15000 // will give error because we cannot update value of const 
+
+console.log("z", z)
