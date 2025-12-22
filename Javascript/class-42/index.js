b64ac5e7616b0e28f98e3f70a580234d12852492
noorsiddiqui-prog <noorsiddiqui048@gmail.com>
2025-12-22 10:43:24 +0500

    feat: class 42 material added

---
+++ Javascript/class-42/index.js
@@ -0,0 +1,60 @@
+// Data types
+
+// 1. Number
+
+var x = 78643549579;
+
+var bInt = BigInt("34750834058305834095834085805804358");
+
+var name = "Noor";
+
+var isFemaleGender = true;
+
+var age;
+
+var batch = null;
+
+// Student
+
+var std1Data = {
+  name: "Noor",
+  age: 100,
+  batch: "SMIT-18",
+  phone: +92-7394792847,
+  job: "SMIT Trainer",
+  address: "xyz street",
+};
+
+var std2Data = {
+  name: "Sana",
+  age: 1000,
+  batch: "SMIT-18",
+  phone: "42342434",
+  job: "SMIT Student",
+  address: "abc street",
+};
+
+
+// Cards objects
+
+const card_one = {
+    image: "https://picture.com",
+    category: "Technology",
+    title: "What's New in 2025 Tech",
+    desc: "lorem ipsum description",
+}
+
+
+
+
+// Naming conventions
+
+// pascal case = PascalCase
+// camel case = studentData
+// snack case = noor_data
+// upper snack case = API_KEY
+
+var noordatabatch18smit = null;
+var student = null
+// var 123Num = 0; // DONOT start a variable with number
+const API_KEY = "secretkey2397498237498237497";
