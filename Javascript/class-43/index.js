c682b9cf0fe2cff3e74ae4aabd3b62ee7da89758
noorsiddiqui-prog <noorsiddiqui048@gmail.com>
2025-12-24 10:55:12 +0500

    feat: class 43 material added

---
+++ Javascript/class-43/index.js
@@ -0,0 +1,26 @@
+// NAN -> not a number
+
+const result = 0 / 0
+
+console.log("result", result)
+
+const arr = ["apple" , "mango", true, 45535, null, undefined, NaN, "orange", "banana", {}]
+
+
+console.log("fruits", arr)
+
+const newArray = new Array("apple" , "mango", "cherry")
+
+console.log("newArray", newArray)
+
+const std1 = {
+    name: "Noor",
+    age: 1000,
+    job: "SMIT trainer",
+    favoriteFruits: ["apple", "banana", "kiwi"]
+}
+
+console.log("std1", std1)
+const multipleDataTypeList = ["apple", "cherry", null, undefined, true, false, NaN, ["one", 3475, "hello", ["cat", "49597"]], { name: "Noor", age: 400, "job": "SMIT trainer"}]
+
+console.log("multipleDataTypeList", multipleDataTypeList)
---
+++ Javascript/class-43/script.js
@@ -0,0 +1,29 @@
+const age = 50
+const question = "What is my age?"
+
+// console.log("question", question, "My age is", age, "years old")
+// console.log("question", question + "My age is" + age + "years old")
+
+
+// const num1 = "10"
+// const num2 = "20"
+
+// console.log("numbers", num1 + num2 ) // concatenation
+// console.log("numbers", num1 / num2 )
+// console.log("numbers", num1 * num2 )
+
+const firstName = "Noor Ul Ain"
+const lastName = "Siddiqui"
+
+
+console.log("Name:", firstName + " " + lastName)
+
+// literals
+
+// console.log(`literals: Name: ${firstName + lastName}`)
+console.log(`literals: Name: ${firstName} ${lastName}`)
+
+console.log(` Question: ${question}, I am ${age} years old `)
+
+
+// Type conversion and operators
