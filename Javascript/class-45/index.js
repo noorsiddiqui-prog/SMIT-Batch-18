a8a6f78d329da4cdedbafaac19323aea873150dd
noorsiddiqui-prog <noorsiddiqui048@gmail.com>
2025-12-31 10:41:25 +0500

    feat: extra files added

---
+++ Javascript/class-45/index.js
@@ -0,0 +1,88 @@
+// // var a = 10
+// // var b = a
+// // var c = b
+
+// // console.log("variables", a, b, c)
+
+// // // chaining assignment
+// // var a = b = c = 4
+
+// // console.log("variable", a, b, c)
+
+// var num = 10
+// // console.log("num ", num = num + 1)
+
+// // increment
+// console.log("num", ++num) //var num = num + 1
+
+// // decrement
+
+// var decNum = 10
+
+// var decNum2 = decNum - 1
+
+// console.log("decrement", --decNum);
+
+
+// console.log("dec", decNum, decNum2)
+
+
+// Chaining Assignment
+var apple = 10
+var banana = 10
+var cherry = 10
+
+var apple = 10
+var banana = apple
+var cherry = banana
+
+console.log("fruits ", apple, banana, cherry)
+
+var apple = banana = cherry = 10
+
+
+// Increment
+
+var i = 0
+
+// i = i + 1 // i = 0 + 1 = 1
+// i = i + 1 // i = 1 + 1 = 2
+// i = i + 1 // i = 2 + 1 = 3
+// i = i + 1 // i = 3 + 1 = 4
+// i = i + 1 // i = 4 + 1 = 5
+// i = i + 1 // i = 5 + 1 = 6
+// console.log("increment", i)
+
+
+
+i = ++i // i = 0 + 1 = 1
+console.log("increment", i)
+i = ++i // i = 1 + 1 = 2
+console.log("increment", i)
+i = ++i // i = 2 + 1 = 3
+console.log("increment", i)
+i = ++i // i = 3 + 1 = 4
+console.log("increment", i)
+i = ++i // i = 4 + 1 = 5
+console.log("increment", i)
+i = ++i // i = 5 + 1 = 6
+console.log("increment", i)
+
+console.log("======================================")
+
+// Decrement
+i = --i // i = 6 - 1 = 5
+console.log("decrement", i)
+i = --i // i = 5 - 1 = 4
+console.log("decrement", i)
+i = --i // i = 4 - 1 = 3
+console.log("decrement", i)
+i = --i // i = 3 + 1 = 2
+console.log("decrement", i)
+i = --i // i = 2 + 1 = 1
+console.log("decrement", i)
+i = --i // i = 1 + 1 = 0
+console.log("decrement", i)
+
+
+
---
+++ Javascript/class-45/script.js
@@ -0,0 +1,31 @@
+console.log("type conversion", "" + 1 + 60)
+
+console.log("type check", typeof(""))
+
+console.log("type conversion", "" - 1  + 8000)
+
+console.log("type check",typeof("" - 1 + 0) )
+
+console.log("false and true", true + true + false)
+
+console.log("divide", 6 / "3");
+
+console.log("concatenation", 4 + 5 + "px");
+
+console.log("ajib expression", "4px" - 2);
+console.log("ajib expression", "4px" - "2px");
+
+
+console.log("spacing expression", "  -9  " + 5);
+console.log("spacing expression", "  -9  " - 5);
+
+console.log("null expression", null + 1);
+console.log("undefined expression", undefined + 1);
+
+
+console.log("string3455", typeof(" \t \n"));
+
+
+
+
+console.log("concatenation", 4 + 5 + "\t px");
