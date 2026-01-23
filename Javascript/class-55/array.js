8a154f314143ca06ea31a1a1a0f7bb30162e2463
noorsiddiqui-prog <noorsiddiqui048@gmail.com>
2026-01-23 11:02:21 +0500

    feat: class 55 material added

---
+++ Javascript/class-55/array.js
@@ -0,0 +1,47 @@
+const fruit1 = "apple";
+const fruit2 = "mango";
+const fruit3 = "cherry";
+const fruit4 = "banana";
+const fruit5 = "pineapple";
+const fruit6 = "grapes";
+
+// const basket = ["apple", "banana", "cherry", "pineapple"];
+
+const basket = [
+  "Apple", "Banana", "Orange", "Mango", "PawPaw", "Strawberry", "Grape", "Kiwi", "Blueberry", "Raspberry",
+  "Peach", "Pear", "Plum", "Cherry", "Watermelon", "Cantaloupe", "Honeydew", "Papaya", "Dragonfruit", "Passionfruit",
+  "Lemon", "Lime", "Grapefruit", "Tangerine", "Mandarin", "Orange", "Clementine", "Kumquat", "Pomelo", "Yuzu",
+  "Avocado", "Coconut", "Guava", "Lychee", "Mangosteen", "Durian", "Jackfruit", "Starfruit", "Persimmon", "Quince",
+  "Fig", "Date", "Apricot", "Nectarine", "Pomegranate", "Kiwiberry", "Blackberry", "Boysenberry", "Elderberry", "Mulberry",
+  "Cranberry", "Gooseberry", "Currant", "Marionberry", "Cloudberry", "Huckleberry", "Salmonberry", "Tayberry", "Juneberry", "Acai",
+  "Plantain", "Breadfruit", "Ugli Fruit", "Custard Apple", "Soursop", "Rambutan", "Longan", "Langsat", "Santol", "Salak",
+  "Feijoa", "Jabuticaba", "Spanish Lime", "Rose Apple", "Water Apple", "Malay Apple", "Sapodilla", "Medlar", "Loquat", "Pawpaw",
+  "Prickly Pear", "Cactus Fruit", "Miracle Fruit", "Goji Berry", "Barberry", "Sea Buckthorn", "Indian Fig", "Jujube", "Mamey Sapote", "Canistel",
+  "Akebia", "Bael", "Bilberry", "Chokeberry", "Crowberry", "Damson", "Gac", "Jambolan", "Nance", "pineapple"
+];
+
+
+
+const extractedFruit = basket[3]
+console.log("extractedFruit", extractedFruit)
+
+// basket[4] = "Grapes"
+basket[0] = "Grapes"
+
+console.log("basket", basket)
+
+const length = basket.length
+console.log("length", length)
+
+console.log("last Index", basket[length - 1]) // basket[3]
+
+
+
+const flashSale = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+]
---
+++ Javascript/class-55/index.js
@@ -0,0 +1,29 @@
+// let a;
+// console.log("a", a)
+
+// a = 10
+// console.log("a", a)
+
+// let b = 20;
+
+// let c = 30
+
+// const age = 50
+
+// if(age > 18){
+//     console.log("age", age)
+//     // let and const
+//     let name = "Noor"
+//     console.log("name", name)
+// }
+
+// // console.log("name", name)
+
+// var age = 50
+const isAdmin = "Noor";
+if (isAdmin) {
+  console.log("age1", age);
+  var age = 50;
+  console.log("age2", age);
+}
+console.log("age3", age);
